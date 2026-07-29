c62eb234d7c6c5cb1d96487026e304eaf949e31c
Lyla Alice Estrada Pulido <lylaxtraw@gmail.com>
2026-07-28 18:50:33 -0600

    refactor(db): añadido función generadora get_db() que FastAPI usará para entregar una sesión limpia por cada request y cerrarla después

--- app/db.py
+++ app/db.py
@@ -1,14 +1,18 @@
 # app/db.py
+from typing import Generator
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
 
-# 1. El Engine: Es el driver de bajo nivel que habla con la base de datos (nuestro "periférico")
-# connect_args={"check_same_thread": False} es necesario para SQLite en FastAPI (que es asíncrono/multihilo)
 engine = create_engine("sqlite:///sensorhub.db", connect_args={"check_same_thread": False})
-
-# 2. SessionLocal: Es la fábrica de sesiones. Una sesión es como una "transacción en memoria"
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
 
-# 3. Base: La clase madre de la que heredarán todos nuestros modelos ORM
 class Base(DeclarativeBase):
     pass
+
+# Generador de sesión inyectable
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
