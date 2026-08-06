06eb9aa85ff64642f6a1ec8810edc6754643b5a2
Lyla Alice Estrada Pulido <lylaxtraw@gmail.com>
2026-08-05 21:14:21 -0600

    fix: arreglado códigos para funcionamiento de URL

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,13 @@
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from alembic import context
 from app.db import Base, get_database_url
+from app.models.reading import ReadingModel
+from app.models.sensor import (
+    SensorModel,  # Asegúrate de que estos nombres coincidan con tus archivos
+)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,14 +22,14 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
-
-config.set_main_option("sqlalchemy.url", get_database_url())
+target_metadata = Base.metadata  # Use the metadata from your Base class
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+config.set_main_option("sqlalchemy.url", get_database_url())
+
 
 
 def run_migrations_offline() -> None:
